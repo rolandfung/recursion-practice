390b596e65c715014e202fe6f7a6905477e16a94
Roland Fung <roland.c.fung@gmail.com>
2016-01-28 23:25:25 -0800

    Add solution without inner function

--- numToText.js
+++ numToText.js
@@ -9,5 +9,32 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 */
 
 var numToText = function(str) {
-
+  if (str.length > 1){
+    return numToText(str[0]).concat(numToText(str.slice(1,str.length)))
+  } else if (str.length === 1) {
+    switch (str) {
+      case '0':
+        return 'zero';
+      case '1':
+        return 'one';
+      case '2':
+        return 'two';
+      case '3':
+        return 'three';
+      case '4':
+        return 'four';
+      case '5':
+        return 'five';
+      case '6':
+        return 'six';
+      case '7':
+        return 'seven';
+      case '8':
+        return 'eight';
+      case '9':
+        return 'nine';
+      default:
+        return str;
+    }
+  }
 };
